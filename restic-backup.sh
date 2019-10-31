5137f68bdbf4acdd4e9db492f43f1434e6829fc7
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2019-10-31 02:50:17 +0100

    add wait-for-it to wait for docker dns query success

--- restic-backup.sh
+++ restic-backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+
+
 # we init the repo if it doesn't exist
 restic -r "${RESTIC_REPOSITORY}" init || true
 
-mysqldump -h "${MYSQL_HOST}" -B "${MYSQL_DATABASE}" -u "${MYSQL_USER}" --password="${MYSQL_PASSWORD}" | restic -r "${RESTIC_REPOSITORY}" backup --stdin --stdin-filename "${BACKUP_FILENAME}.sql"
+/wait-for-it.sh ${MYSQL_HOST}:3306 --strict -t 60 -- mysqldump -h "${MYSQL_HOST}" -B "${MYSQL_DATABASE}" -u "${MYSQL_USER}" --password="${MYSQL_PASSWORD}" | restic -r "${RESTIC_REPOSITORY}" backup --stdin --stdin-filename "${BACKUP_FILENAME}.sql"
